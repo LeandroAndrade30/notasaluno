2cdfa69fc616b0d4504cc56af4ee8a62abce4f2d
Leandro Andrade <leandrade666@gmail.com>
2024-02-25 16:11:25 -0300

    Criado Projeto Ler tres notas Aluno

---
+++ LerTresNotasAluno/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LerTresNotasAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
