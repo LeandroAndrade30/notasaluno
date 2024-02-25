b67c474a8410a87e3cb497ed74888b7f0fdec4e8
Leandro Andrade <leandrade666@gmail.com>
2024-02-25 18:11:30 -0300

    Criado Versao do Programa Ler tres notas aluno sem Orientação a Objetos

--- LerTresNotasAluno/Program.cs
+++ LerTresNotasAluno/Program.cs
@@ -6,7 +6,32 @@ namespace LerTresNotasAluno
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Informe o Nome do Aluno");
+            string Nome = Console.ReadLine();
+            Console.WriteLine("Informe a Nota do  Aluno no Primeiro Trimestre");
+            float Nota1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a Nota do  Aluno no Primeiro Segundo");
+            float Nota2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a Nota do  Aluno no Primeiro Terceiro");
+            float Nota3 = float.Parse(Console.ReadLine());
+            float MediaPonderada = (Nota1 * 30  + Nota2 * 35  + Nota3 * 35) / 100;
+            Console.WriteLine($"A Média do Aluno {Nome} é: {MediaPonderada}");
+
+             if(MediaPonderada>=60)
+            {
+                Console.WriteLine("\nAluno Aprovado");
+            }
+             else
+            {
+                Console.WriteLine("\nAluno Reprovado");
+
+                float NotaFaltando = (60 - MediaPonderada);
+
+                Console.WriteLine($"\nFaltando {NotaFaltando} para o aluno ser aprovado");
+
+            }
+
+
         }
     }
 }
