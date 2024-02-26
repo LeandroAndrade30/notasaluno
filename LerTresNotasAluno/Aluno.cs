54c5b4472839d7ee11567deedac8dd64089371c8
Leandro Andrade <leandrade666@gmail.com>
2024-02-26 13:38:56 -0300

    Criada a versão do Programa Ler tres notas aluno com orientação a objetos

---
+++ LerTresNotasAluno/Aluno.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LerTresNotasAluno
+{
+    class Aluno
+    {
+        public string Nome;
+        public float Nota1;
+        public float Nota2;
+        public float Nota3;
+        public float MediaPonderada;
+
+
+
+
+        public  float CalcularMediaPonderada()
+        {
+            MediaPonderada = (Nota1 * 0.3f + Nota2 * 0.35f + Nota3 * 0.35f ) /1;
+            return MediaPonderada;
+
+        }
+
+        public static  float MediaAluno(Aluno Aluno)
+
+        {
+            
+
+            Console.WriteLine($"A Média do Aluno {Aluno.Nome} é: {Aluno.MediaPonderada}");
+
+            if (Aluno.MediaPonderada >= 60)
+            {
+                Console.WriteLine("\n Aluno Aprovado\n");
+            }
+            else
+            {
+                Console.WriteLine("\n Aluno Reprovado\n");
+
+                float NotaFaltando = (60 - Aluno.MediaPonderada);
+
+                Console.WriteLine($"Faltando {NotaFaltando } para o aluno {Aluno.Nome} ser aprovado");
+                
+            }
+            return Aluno.MediaPonderada;
+            
+        }
+    }
+}
+
--- LerTresNotasAluno/Program.cs
+++ LerTresNotasAluno/Program.cs
@@ -6,32 +6,23 @@ namespace LerTresNotasAluno
     {
         static void Main(string[] args)
         {
+            Aluno Aluno = new Aluno();
+
             Console.WriteLine("Informe o Nome do Aluno");
-            string Nome = Console.ReadLine();
+             Aluno.Nome = Console.ReadLine();
             Console.WriteLine("Informe a Nota do  Aluno no Primeiro Trimestre");
-            float Nota1 = float.Parse(Console.ReadLine());
+            Aluno.Nota1 = float.Parse(Console.ReadLine());
             Console.WriteLine("Informe a Nota do  Aluno no Primeiro Segundo");
-            float Nota2 = float.Parse(Console.ReadLine());
+            Aluno.Nota2 = float.Parse(Console.ReadLine());
             Console.WriteLine("Informe a Nota do  Aluno no Primeiro Terceiro");
-            float Nota3 = float.Parse(Console.ReadLine());
-            float MediaPonderada = (Nota1 * 30  + Nota2 * 35  + Nota3 * 35) / 100;
-            Console.WriteLine($"A Média do Aluno {Nome} é: {MediaPonderada}");
-
-             if(MediaPonderada>=60)
-            {
-                Console.WriteLine("\nAluno Aprovado");
-            }
-             else
-            {
-                Console.WriteLine("\nAluno Reprovado");
-
-                float NotaFaltando = (60 - MediaPonderada);
-
-                Console.WriteLine($"\nFaltando {NotaFaltando} para o aluno ser aprovado");
-
-            }
-
+            Aluno.Nota3= float.Parse(Console.ReadLine());
+            float MediaPonderada = Aluno.CalcularMediaPonderada();
+            float mediaAluno = Aluno.MediaAluno(Aluno);
+            
+            
 
         }
     }
 }
+
+
